99a8ead3424cf68a74c28686acde152ee7c9e49a
Dmitry Baryshnikov <bishop.dev@gmail.com>
2018-11-04 01:34:34 +0300

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -34,7 +34,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
-    'sphinx.ext.pngmath',
+    'sphinx.ext.imgmath',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
 ]
@@ -55,7 +55,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'NextGIS Open geodata portal'
-copyright = u'2016, NextGIS'
+copyright = u'2016-2018, NextGIS'
 author = u'Команда NextGIS'
 
 # The version info for the project you're documenting, acts as replacement for
